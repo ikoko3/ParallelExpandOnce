59f01eef324293600de88b672f2ccc1eca5f83cd
it21521 <it21521@hua.gr>
2021-01-05 01:47:58 +0200

    Removed existing code from tutorial

--- ExpandOnce/ExpandOnce.cpp
+++ ExpandOnce/ExpandOnce.cpp
@@ -37,91 +37,7 @@ using namespace std;
 using namespace tbb;
 
 
-static const size_t N = 23;
 static const int threshold = 2;
-
-class SubStringFinder {
-
-	const string str;
-	size_t * max_array;
-	size_t * pos_array;
-
-public:
-
-		void operator() (const blocked_range<size_t>& r) const {
-		for (size_t i = r.begin(); i != r.end(); ++i) {
-			size_t max_size = 0, max_pos = 0;
-			for (size_t j = 0; j < str.size(); ++j)
-				if (j != i) {
-					size_t limit = str.size() - max(i, j);
-					for (size_t k = 0; k < limit; ++k) {
-						if (str[i + k] != str[j + k]) break;
-						if (k > max_size) {
-							max_size = k;
-							max_pos = j;
-						}
-					}
-				}
-
-			max_array[i] = max_size;
-			pos_array[i] = max_pos;
-		}
-		
-	}
-
-	SubStringFinder(string &s, size_t *m, size_t *p) :
-		str(s), max_array(m), pos_array(p) { }
-
-
-};
-
-int serial() {
-	string str[N] = { string("a"), string("b") };
-	for (size_t i = 2; i < N; ++i) str[i] = str[i - 1] + str[i - 2];
-	string & to_scan = str[N - 1];
-	size_t num_elem = to_scan.size();
-	size_t * max = new size_t[num_elem];
-
-
-	size_t * pos = new size_t[num_elem];
-
-
-	// add code to populate max and pos here
-
-	for (size_t i = 0; i < num_elem; ++i)
-		cout << " " << max[i] << "(" << pos[i] << ")" << endl;
-
-	delete[] pos;
-
-
-	delete[] max;
-	return 0;
-
-}
-
-int parallel() {
-
-	string str[N] = { string("a"), string("b") };
-	for (size_t i = 2; i < N; ++i) str[i] = str[i - 1] + str[i - 2];
-	string & to_scan = str[N - 1];
-
-	size_t num_elem = to_scan.size();
-
-	size_t * max = new size_t[num_elem];
-	size_t * pos = new size_t[num_elem];
-
-
-
-	parallel_for(blocked_range<size_t>(0, num_elem),
-		SubStringFinder(to_scan, max, pos));
-
-	for (size_t i = 0; i < num_elem; ++i)
-		cout << " " << max[i] << "(" << pos[i] << ")" << endl;
-	delete[] pos;
-	delete[] max;
-	return 0;
-
-}
 
 using namespace csr;
 
@@ -134,6 +50,8 @@ int main() {
 
 	SeedSet* set = gh::CreateSeedSetFromFile(GraphFilesConfig::GetNoisySeedSetName());
 	set->print();
+
+
 
 
 
