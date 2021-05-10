97af7877f385771ac400804d3b6bfbc8b9dc6412
it21521 <it21521@hua.gr>
2021-05-10 20:01:12 +0300

    f1 & recall

--- visualize.py
+++ visualize.py
@@ -8,9 +8,11 @@ json_path = "C:\\Users\\john_\\Documents\\tbb_env\\"
 
 noisy_seeds_results = csv_path + "NoisySeeds.csv"
 expand_once_results = csv_path + "expandOnce.csv"
+test_results = csv_path + "tests.csv"
 
-file = noisy_seeds_results
+#file = noisy_seeds_results
 #file = expand_once_results
+file = test_results
 
 
 def normalize_data(df):
@@ -65,17 +67,37 @@ def compare_avg_accuracy(df, min_treshold):
     print(f"Parallel accuracy Mean:  {parallel_acc_mean}")
     print(f"Improvement: {improvement}")
 
-names = ['threshold','SeedSetSize','Nodes','G1Edges','G2Edges','Stime','Saccuracy','Scorrect','Stotal','Ptime','Paccuracy','Pcorrect','Ptotal'] 
+def get_Srecall(row):
+    return row['Scorrect'] / row['nIdent']
+
+def get_Precall(row):
+    return row['Pcorrect'] / row['nIdent']
+
+def get_Sf1(row):
+    prec = row["Saccuracy"]
+    recall = get_Srecall(row)
+    return 2 * (prec*recall/(prec+recall))
+
+def get_Pf1(row):
+    prec = row["Paccuracy"]
+    recall = get_Precall(row)
+    return 2 * (prec*recall/(prec+recall))
+
+names = ['threshold','SeedSetSize','Nodes','G1Edges','G2Edges','nIdent','Stime','Saccuracy','Scorrect','Stotal','Ptime','Paccuracy','Pcorrect','Ptotal'] 
 df = pd.read_csv(file, names=names,index_col=False)
 
 
 selection = df.loc[:2,['Nodes','Stime','Ptime']]
 
-normalize_data(df)
+for i,row in df.iterrows():
+    print(f"SR:{get_Srecall(row)} - PR:{get_Precall(row)} - SF1:{get_Sf1(row)} - PF1:{get_Pf1(row)}")
+
+
+#normalize_data(df)
 #print_density_time(df)
 #print_threshold_accuracy(df)
 #print_matrix(df)
 #calculate_acceleration(df)
-compare_avg_accuracy(df,3)
+#compare_avg_accuracy(df,3)
 #print(df)
 
